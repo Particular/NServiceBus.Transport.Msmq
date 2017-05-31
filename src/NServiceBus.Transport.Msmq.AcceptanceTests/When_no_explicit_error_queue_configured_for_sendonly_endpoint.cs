ba4da06afc2a715494cc067536b26242020fec2f
Sean Feldman <feldman.sean@gmail.com>
2017-05-31 17:04:40 -0600

    red test

---
+++ src/NServiceBus.Transport.Msmq.AcceptanceTests/When_no_explicit_error_queue_configured_for_sendonly_endpoint.cs
@@ -0,0 +1,35 @@
+﻿namespace NServiceBus.Transport.Msmq.AcceptanceTests
+{
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NUnit.Framework;
+
+    public class When_no_explicit_error_queue_configured_for_sendonly_endpoint : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_start_endpoint()
+        {
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                await Scenario.Define<Context>()
+                    .WithEndpoint<EndpointWithNoErrorQConfig>()
+                    .Run();
+            });
+        }
+
+        public class EndpointWithNoErrorQConfig : EndpointConfigurationBuilder
+        {
+            public EndpointWithNoErrorQConfig()
+            {
+                EndpointSetup<ServerWithNoErrorQueueConfigured>(endpointConfiguration =>
+                {
+                    endpointConfiguration.SendOnly();
+                });
+            }
+        }
+
+        public class Context : ScenarioContext
+        {
+        }
+    }
+}
