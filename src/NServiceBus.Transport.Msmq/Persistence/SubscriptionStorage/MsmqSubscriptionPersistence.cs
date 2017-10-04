b6a15b9d71f8196cb63c9481cc53fc3db0a6681e
indualagarsamy <indu.alagarsamy@particular.net>
2017-10-04 07:47:28 -0700

    If by any chance there is an older "NServiceBus.Subscriptions" queue that is in use, we now explicitly warn the user to move to the new queue which is [EndpointName].Subscriptions by throwing an exception rather than silently use the new default.

--- src/NServiceBus.Transport.Msmq/Persistence/SubscriptionStorage/MsmqSubscriptionPersistence.cs
+++ src/NServiceBus.Transport.Msmq/Persistence/SubscriptionStorage/MsmqSubscriptionPersistence.cs
@@ -1,5 +1,7 @@
 ﻿namespace NServiceBus.Persistence.Msmq
 {
+    using System;
+    using System.Messaging;
     using Features;
     using Logging;
     using Transport;
@@ -9,15 +11,24 @@
     {
         protected override void Setup(FeatureConfigurationContext context)
         {
-            var queueName = context.Settings.GetConfiguredMsmqPersistenceSubscriptionQueue();
+            var configuredQueueName = context.Settings.GetConfiguredMsmqPersistenceSubscriptionQueue();
 
-            if (string.IsNullOrEmpty(queueName))
+            if (string.IsNullOrEmpty(configuredQueueName))
             {
-                queueName = $"{context.Settings.EndpointName()}.Subscriptions";
-                Logger.Warn($"The queue used to store subscriptions has not been configured, so the default '{queueName}' will be used.");
+                if (DoesOldDefaultQueueExists())
+                {
+                    // The user has not configured the subscriptions queue to be "NServiceBus.Subscriptions" but there's a local queue. 
+                    // Indicates that the endoint was using old default queue name.
+                    throw new Exception(
+                        "Detected the presence of an old default queue named `NServiceBus.Subscriptions`. The new default is now `[Your endpointname].Subscriptions`. Move the messages to the new queue or configure the subscriptions queue name.");
+                }
+
+                var defaultQueueName = $"{context.Settings.EndpointName()}.Subscriptions";
+                Logger.Warn($"The queue used to store subscriptions has not been configured, so the default '{defaultQueueName}' will be used.");
+                configuredQueueName = defaultQueueName;
             }
 
-            context.Settings.Get<QueueBindings>().BindSending(queueName);
+            context.Settings.Get<QueueBindings>().BindSending(configuredQueueName);
 
             var useTransactionalStorageQueue = true;
             MsmqSettings msmqSettings;
@@ -29,9 +40,16 @@
 
             context.Container.ConfigureComponent(b =>
             {
-                var queue = new MsmqSubscriptionStorageQueue(MsmqAddress.Parse(queueName), useTransactionalStorageQueue);
+                var queue = new MsmqSubscriptionStorageQueue(MsmqAddress.Parse(configuredQueueName), useTransactionalStorageQueue);
                 return new MsmqSubscriptionStorage(queue);
             }, DependencyLifecycle.SingleInstance);
+        }
+
+        bool DoesOldDefaultQueueExists()
+        {
+            const string oldDefaultSubscriptionsQueue = "NServiceBus.Subscriptions";
+            var path = MsmqAddress.Parse(oldDefaultSubscriptionsQueue).PathWithoutPrefix;
+            return MessageQueue.Exists(path);
         }
 
         static ILog Logger = LogManager.GetLogger(typeof(MsmqSubscriptionPersistence));
