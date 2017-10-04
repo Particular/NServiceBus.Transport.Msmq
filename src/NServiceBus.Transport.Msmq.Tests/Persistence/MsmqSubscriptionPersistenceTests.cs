48fde0a090c1addc8e00ad710c05bf27b8db88d5
indualagarsamy <indu.alagarsamy@particular.net>
2017-10-04 13:17:12 -0700

    cleaned up unit tests.

--- src/NServiceBus.Transport.Msmq.Tests/Persistence/MsmqSubscriptionPersistenceTests.cs
+++ src/NServiceBus.Transport.Msmq.Tests/Persistence/MsmqSubscriptionPersistenceTests.cs
@@ -7,36 +7,38 @@
     [TestFixture]
     public class MsmqSubscriptionPersistenceTests
     {
-        [Test]
-        public void ShouldThrowIfOldDefaultSubscriptionQueuePresent()
+        const string oldDefaultQueue = "NServiceBus.Subscriptions";
+
+        [SetUp]
+        public void Setup()
         {
             // Create queue "NServiceBus.Subscriptions" to simulate the presence of the old default queue. 
-            const string oldDefaultQueue = "NServiceBus.Subscriptions";
             var queuePath = MsmqAddress.Parse(oldDefaultQueue).PathWithoutPrefix;
             MsmqHelpers.CreateQueue(queuePath);
+        }
 
-            var persistence = new MsmqSubscriptionPersistence();
-            Assert.IsTrue(persistence.DoesOldDefaultQueueExists());
-            Assert.Throws<Exception>(() => persistence.ThrowIfUsingTheOldDefaultSubscriptionsQueue(""));
-
+        [TearDown]
+        public void TearDown()
+        {
+            var queuePath = MsmqAddress.Parse(oldDefaultQueue).PathWithoutPrefix;
             // Delete the queue
             MsmqHelpers.DeleteQueue(queuePath);
         }
 
         [Test]
+        public void ShouldThrowIfOldDefaultSubscriptionQueuePresent()
+        {
+            var persistence = new MsmqSubscriptionPersistence();
+            Assert.IsTrue(persistence.DoesOldDefaultQueueExists());
+            Assert.Throws<Exception>(() => persistence.ThrowIfUsingTheOldDefaultSubscriptionsQueue(""));
+        }
+
+        [Test]
         public void ShouldNotThrowIfDefaultSubscriptionQueueIsConfigured()
         {
-            // Create queue "NServiceBus.Subscriptions" to simulate the presence of the old default queue. 
-            const string oldDefaultQueue = "NServiceBus.Subscriptions";
-            var queuePath = MsmqAddress.Parse(oldDefaultQueue).PathWithoutPrefix;
-            MsmqHelpers.CreateQueue(queuePath);
-
             var persistence = new MsmqSubscriptionPersistence();
             Assert.IsTrue(persistence.DoesOldDefaultQueueExists());
             Assert.DoesNotThrow(() => persistence.ThrowIfUsingTheOldDefaultSubscriptionsQueue("NServiceBus.Subscriptions"));
-
-            // Delete the queue
-            MsmqHelpers.DeleteQueue(queuePath);
         }
     }
 }
