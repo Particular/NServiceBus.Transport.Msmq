4a8d9f8088bfc5da3a69fe8dc88aa045c19b5a04
Simon Cropp <simon.cropp@gmail.com>
2017-10-06 09:18:24 +1100

    remove usingMsmq from TimeToBeReceivedOverrideChecker
    since we are always in msmq in this context

--- src/NServiceBus.Transport.Msmq/MsmqTimeToBeReceivedOverrideCheck.cs
+++ src/NServiceBus.Transport.Msmq/MsmqTimeToBeReceivedOverrideCheck.cs
@@ -15,14 +15,13 @@
 
         public StartupCheckResult CheckTimeToBeReceivedOverrides()
         {
-            var usingMsmq = settings.Get<TransportDefinition>() is MsmqTransport;
             var isTransactional = settings.GetRequiredTransactionModeForReceives() != TransportTransactionMode.None;
             var outBoxRunning = settings.IsFeatureActive(typeof(Outbox));
 
             settings.TryGetAuditMessageExpiration(out var auditMessageExpiration);
             var auditTTBROverridden = auditMessageExpiration > TimeSpan.Zero;
 
-            return TimeToBeReceivedOverrideChecker.Check(usingMsmq, isTransactional, outBoxRunning, auditTTBROverridden);
+            return TimeToBeReceivedOverrideChecker.Check(isTransactional, outBoxRunning, auditTTBROverridden);
         }
 
         ReadOnlySettings settings;
