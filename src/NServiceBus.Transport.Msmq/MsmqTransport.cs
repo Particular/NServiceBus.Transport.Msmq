71e4311f05b6c01670e0450ec650f9e5d0be8568
Jan Ove Skogheim <janovesk@janovesk.com>
2017-10-25 15:53:04 +0200

    Add MSMQ startup diagnostics

--- src/NServiceBus.Transport.Msmq/MsmqTransport.cs
+++ src/NServiceBus.Transport.Msmq/MsmqTransport.cs
@@ -1,12 +1,12 @@
 namespace NServiceBus
 {
     using System;
+    using System.Messaging;
     using Features;
     using Routing;
     using Settings;
     using Transport;
     using Transport.Msmq;
-    using System.Collections.Generic;
 
     /// <summary>
     /// Transport definition for MSMQ.
@@ -65,13 +65,29 @@ transport.TimeToReachQueue(timespanValue);";
             }
 
             var msmqSettings = new MsmqSettings(settings);
-            
+            settings.AddStartupDiagnosticsSection("NServiceBus.Transport.MSMQ", new
+            {
+                msmqSettings.ExecuteInstaller,
+                msmqSettings.UseDeadLetterQueue,
+                msmqSettings.UseConnectionCache,
+                msmqSettings.UseTransactionalQueues,
+                msmqSettings.UseJournalQueue,
+                msmqSettings.UseDeadLetterQueueForMessagesWithTimeToBeReceived,
+                TimeToReachQueue = GetFormattedTimeToReachQueue(msmqSettings.TimeToReachQueue)
+            });
+
             var isTransactional = IsTransactional(settings);
             var outBoxRunning = settings.IsFeatureActive(typeof(Features.Outbox));
 
             settings.TryGetAuditMessageExpiration(out var auditMessageExpiration);
 
             return new MsmqTransportInfrastructure(msmqSettings, settings.Get<QueueBindings>(), isTransactional, outBoxRunning, auditMessageExpiration);
+        }
+
+        static string GetFormattedTimeToReachQueue(TimeSpan timeToReachQueue)
+        {
+            return timeToReachQueue == Message.InfiniteTimeout ? "Infinite" 
+                : string.Format("{0:%d} day(s) {0:%hh} hours(s) {0:%mm} minute(s) {0:%ss} second(s)", timeToReachQueue);
         }
 
 
