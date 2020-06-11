fe651bb550d33da0cdb1908c502c991f56044f05
Ramon Smits <ramon.smits@gmail.com>
2020-06-11 17:10:16 +0200

    Support reading instance mapping from Uri.

--- src/NServiceBus.Transport.Msmq/InstanceMapping/InstanceMappingFileAccess.cs
+++ src/NServiceBus.Transport.Msmq/InstanceMapping/InstanceMappingFileAccess.cs
@@ -1,5 +1,6 @@
 namespace NServiceBus.Transport.Msmq
 {
+    using System;
     using System.IO;
     using System.Xml;
     using System.Xml.Linq;
@@ -8,10 +9,20 @@ namespace NServiceBus.Transport.Msmq
     {
         public XDocument Load(string path)
         {
-            using (var file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            using (var reader = XmlReader.Create(file))
+            if (Uri.TryCreate(path, UriKind.Absolute, out var uri) && !uri.IsFile)
             {
-                return XDocument.Load(reader);
+                using (var reader = XmlReader.Create(uri.ToString()))
+                {
+                    return XDocument.Load(reader);
+                }
+            }
+            else
+            {
+                using (var file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var reader = XmlReader.Create(file))
+                {
+                    return XDocument.Load(reader);
+                }
             }
         }
     }
