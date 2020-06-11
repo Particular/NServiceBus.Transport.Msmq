91435c21cb515be6df4e9e9a4e05280dd22c6183
Ramon Smits <ramon.smits@gmail.com>
2020-06-11 08:47:01 +0200

    Added Send/Reply/Publish options `UseDeadLetterQueue` and `UseJournalQueue`.

---
+++ src/NServiceBus.Transport.Msmq.AcceptanceTests/When_sending_message_with_journaling.cs
@@ -0,0 +1,140 @@
+﻿namespace NServiceBus.Transport.Msmq.AcceptanceTests
+{
+    using System;
+    using System.Messaging;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using NServiceBus.AcceptanceTests;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NUnit.Framework;
+
+    public class When_sending_message_with_journaling : NServiceBusAcceptanceTest
+    {
+        [Test]
+        [TestCase(false, false, false)]
+        [TestCase(true, true, true)]
+        [TestCase(false, true, true)]
+        [TestCase(true, false, false)]
+        [TestCase(true, null, true)]
+        [TestCase(true, null, true)]
+        [TestCase(false, null, false)]
+        public async Task Verify_dlq_option(bool global, bool? enable, bool result)
+        {
+            DeleteSpyQueue();
+            MessageQueue.Create(sendSpyQueue, true);
+            try
+            {
+                await Scenario.Define<Context>()
+                .WithEndpoint<Endpoint>(b => b.CustomConfig(c =>
+                {
+                    var t = c.UseTransport<MsmqTransport>();
+                    if (!global) t.DisableDeadLetterQueueing();
+                }).When(async (session, c) =>
+                {
+                    var options = new SendOptions();
+                    options.SetDestination(sendSpyEndpoint);
+                    if (enable != null)
+                    {
+                        options.UseDeadLetterQueue(enable: enable.Value);
+                    }
+                    await session.Send(new MyMessage(), options);
+                    await session.Send(new MyMessage(), options);
+                    c.WasCalled = true;
+                }))
+                .Done(c => c.WasCalled)
+                .Run();
+
+                using (var queue = new MessageQueue(sendSpyQueue))
+                {
+                    using (var message = queue.Receive(TimeSpan.FromSeconds(5)))
+                    {
+                        Assert.AreEqual(result, message?.UseDeadLetterQueue, "UseDeadLetterQueue");
+                    }
+                }
+            }
+            finally
+            {
+                //DeleteSpyQueue();
+            }
+        }
+
+        [Test]
+        [TestCase(false, false, false)]
+        [TestCase(true, true, true)]
+        [TestCase(false, true, true)]
+        [TestCase(true, false, false)]
+        [TestCase(true, null, true)]
+        [TestCase(true, null, true)]
+        [TestCase(false, null, false)]
+        public async Task Verify_journaling_option(bool global, bool? enable, bool result)
+        {
+            DeleteSpyQueue();
+            MessageQueue.Create(sendSpyQueue, true);
+            try
+            {
+                await Scenario.Define<Context>()
+                .WithEndpoint<Endpoint>(b => b.CustomConfig(c =>
+                {
+                    var t = c.UseTransport<MsmqTransport>();
+                    if (global) t.EnableJournaling();
+                }).When(async (session, c) =>
+                {
+                    var options = new SendOptions();
+                    options.SetDestination(sendSpyEndpoint);
+                    if (enable != null)
+                    {
+                        options.UseJournalQueue(enable: enable.Value);
+                    }
+                    await session.Send(new MyMessage(), options);
+                    await session.Send(new MyMessage(), options);
+                    c.WasCalled = true;
+                }))
+                .Done(c => c.WasCalled)
+                .Run();
+
+                using (var queue = new MessageQueue(sendSpyQueue))
+                {
+                    using (var message = queue.Receive(TimeSpan.FromSeconds(5)))
+                    {
+                        Assert.AreEqual(result, message?.UseJournalQueue, "UseJournalQueue");
+                    }
+                }
+            }
+            finally
+            {
+                //DeleteSpyQueue();
+            }
+        }
+
+
+        static void DeleteSpyQueue()
+        {
+            if (MessageQueue.Exists(sendSpyQueue))
+            {
+                MessageQueue.Delete(sendSpyQueue);
+            }
+        }
+
+        static string sendSpyEndpoint = "dlqForJournalingSpy";
+        static string sendSpyQueue = $@".\private$\{sendSpyEndpoint}";
+
+        public class Context : ScenarioContext
+        {
+            public bool WasCalled { get; set; }
+        }
+
+        public class Endpoint : EndpointConfigurationBuilder
+        {
+            public Endpoint()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                });
+            }
+        }
+
+        public class MyMessage : IMessage
+        {
+        }
+    }
+}
---
+++ src/NServiceBus.Transport.Msmq/DeadLetterQueueOptionExtensions.cs
@@ -0,0 +1,49 @@
+﻿namespace NServiceBus
+{
+    using Extensibility;
+    using Transport.Msmq;
+
+    /// <summary>
+    /// Gives users fine grained control over routing via extension methods.
+    /// </summary>
+    public static class DeadLetterQueueOptionExtensions
+    {
+        internal const string KeyDeadLetterQueue = "MSMQ.UseDeadLetterQueue";
+
+        /// <summary>
+        /// Allows a to enable or disable MSMQ journaling.
+        /// </summary>
+        /// <param name="options">Option being extended.</param>
+        /// <param name="enable">Either enable or disabling message journaling.</param>
+        public static void UseDeadLetterQueue(this SendOptions options, bool enable)
+        {
+            Guard.AgainstNull(nameof(options), options);
+            var ext = options.GetExtensions();
+            ext.Set(KeyDeadLetterQueue, enable);
+        }
+
+        /// <summary>
+        /// Allows a to enable or disable MSMQ journaling.
+        /// </summary>
+        /// <param name="options">Option being extended.</param>
+        /// <param name="enable">Either enable or disabling message journaling.</param>
+        public static void UseDeadLetterQueue(this ReplyOptions options, bool enable)
+        {
+            Guard.AgainstNull(nameof(options), options);
+            var ext = options.GetExtensions();
+            ext.Set(KeyDeadLetterQueue, enable);
+        }
+
+        /// <summary>
+        /// Allows a to enable or disable MSMQ journaling.
+        /// </summary>
+        /// <param name="options">Option being extended.</param>
+        /// <param name="enable">Either enable or disabling message journaling.</param>
+        public static void UseDeadLetterQueue(this PublishOptions options, bool enable)
+        {
+            Guard.AgainstNull(nameof(options), options);
+            var ext = options.GetExtensions();
+            ext.Set(KeyDeadLetterQueue, enable);
+        }
+    }
+}
