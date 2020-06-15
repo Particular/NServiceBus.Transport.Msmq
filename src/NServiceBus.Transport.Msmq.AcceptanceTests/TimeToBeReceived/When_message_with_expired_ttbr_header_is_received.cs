2faaa268d6b6e0e017e6ccb0e59cd416487aac67
Mike Minutillo <mike.minutillo@particular.net>
2020-06-15 16:56:54 +0800

    Remove redundtant qualifier

--- src/NServiceBus.Transport.Msmq.AcceptanceTests/TimeToBeReceived/When_message_with_expired_ttbr_header_is_received.cs
+++ src/NServiceBus.Transport.Msmq.AcceptanceTests/TimeToBeReceived/When_message_with_expired_ttbr_header_is_received.cs
@@ -7,7 +7,7 @@
     using NUnit.Framework;
     using System;
     using System.Threading.Tasks;
-    using NServiceBus.Configuration.AdvancedExtensibility;
+    using Configuration.AdvancedExtensibility;
 
     class When_message_with_expired_ttbr_header_is_received : NServiceBusAcceptanceTest
     {
