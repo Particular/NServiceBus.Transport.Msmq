dc0291f07201920ebed7ddddda98d90104944365
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-03-05 19:05:42 +0100

    update tests

--- src/NServiceBus.Transport.Msmq.Tests/MsmqFailureInfoStorageTests.cs
+++ src/NServiceBus.Transport.Msmq.Tests/MsmqFailureInfoStorageTests.cs
@@ -2,6 +2,7 @@ namespace NServiceBus.Transport.Msmq.Tests
 {
     using System;
     using System.Collections.Generic;
+    using Extensibility;
     using NUnit.Framework;
 
     public class MsmqFailureInfoStorageTests
@@ -14,7 +15,7 @@ namespace NServiceBus.Transport.Msmq.Tests
 
             var storage = GetFailureInfoStorage();
 
-            storage.RecordFailureInfoForMessage(messageId, exception);
+            storage.RecordFailureInfoForMessage(messageId, exception, new ContextBag());
 
             storage.TryGetFailureInfoForMessage(messageId, out var failureInfo);
 
@@ -31,8 +32,8 @@ namespace NServiceBus.Transport.Msmq.Tests
 
             var storage = GetFailureInfoStorage();
 
-            storage.RecordFailureInfoForMessage(messageId, new Exception());
-            storage.RecordFailureInfoForMessage(messageId, secondException);
+            storage.RecordFailureInfoForMessage(messageId, new Exception(), new ContextBag());
+            storage.RecordFailureInfoForMessage(messageId, secondException, new ContextBag());
 
             storage.TryGetFailureInfoForMessage(messageId, out var failureInfo);
 
@@ -48,7 +49,7 @@ namespace NServiceBus.Transport.Msmq.Tests
 
             var storage = GetFailureInfoStorage();
 
-            storage.RecordFailureInfoForMessage(messageId, new Exception());
+            storage.RecordFailureInfoForMessage(messageId, new Exception(), new ContextBag());
 
             storage.TryGetFailureInfoForMessage(messageId, out var failureInfo);
             Assert.NotNull(failureInfo);
@@ -62,19 +63,19 @@ namespace NServiceBus.Transport.Msmq.Tests
         [Test]
         public void When_recording_more_than_max_number_of_failures_should_remove_least_recently_used_entry()
         {
-            const int MaxElements = 50;
-            var storage = new MsmqFailureInfoStorage(maxElements: MaxElements);
+            const int maxElements = 50;
+            var storage = new MsmqFailureInfoStorage(maxElements: maxElements);
 
             var lruMessageId = Guid.NewGuid().ToString("D");
 
-            storage.RecordFailureInfoForMessage(lruMessageId, new Exception());
+            storage.RecordFailureInfoForMessage(lruMessageId, new Exception(), new ContextBag());
 
-            for (var i = 0; i < MaxElements; ++i)
+            for (var i = 0; i < maxElements; ++i)
             {
                 var messageId = Guid.NewGuid().ToString("D");
                 var exception = new Exception();
 
-                storage.RecordFailureInfoForMessage(messageId, exception);
+                storage.RecordFailureInfoForMessage(messageId, exception, new ContextBag());
             }
 
             storage.TryGetFailureInfoForMessage(lruMessageId, out var failureInfo);
@@ -85,27 +86,27 @@ namespace NServiceBus.Transport.Msmq.Tests
         [Test]
         public void When_recording_a_failure_for_a_message_it_should_not_be_treated_as_least_recently_used()
         {
-            const int MaxElements = 50;
-            var storage = new MsmqFailureInfoStorage(MaxElements);
+            const int maxElements = 50;
+            var storage = new MsmqFailureInfoStorage(maxElements);
 
             var lruMessageId = Guid.NewGuid().ToString("D");
 
-            storage.RecordFailureInfoForMessage(lruMessageId, new Exception());
+            storage.RecordFailureInfoForMessage(lruMessageId, new Exception(), new ContextBag());
 
-            var messageIds = new List<string>(MaxElements);
-            for (var i = 0; i < MaxElements; ++i)
+            var messageIds = new List<string>(maxElements);
+            for (var i = 0; i < maxElements; ++i)
             {
                 messageIds.Add(Guid.NewGuid().ToString("D"));
             }
 
-            for (var i = 0; i < MaxElements - 1; ++i)
+            for (var i = 0; i < maxElements - 1; ++i)
             {
-                storage.RecordFailureInfoForMessage(messageIds[i], new Exception());
+                storage.RecordFailureInfoForMessage(messageIds[i], new Exception(), new ContextBag());
             }
 
-            storage.RecordFailureInfoForMessage(lruMessageId, new Exception());
+            storage.RecordFailureInfoForMessage(lruMessageId, new Exception(), new ContextBag());
 
-            storage.RecordFailureInfoForMessage(messageIds[MaxElements - 1], new Exception());
+            storage.RecordFailureInfoForMessage(messageIds[maxElements - 1], new Exception(), new ContextBag());
 
             storage.TryGetFailureInfoForMessage(lruMessageId, out var failureInfo);
 
