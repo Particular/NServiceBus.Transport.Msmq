87268b370df9f052d55d1ba6e8b542cc48580773
Christian <christian.quiros+github@gmail.com>
2022-04-21 10:37:35 -0600

    Update src/NServiceBus.Transport.Msmq/Utils/ContextExtensions.cs
    Co-authored-by: Tim Bussmann <timbussmann@users.noreply.github.com>

--- src/NServiceBus.Transport.Msmq/Utils/ContextExtensions.cs
+++ src/NServiceBus.Transport.Msmq/Utils/ContextExtensions.cs
@@ -4,6 +4,7 @@
 
     static class ContextExtensions
     {
+        // Implements the 'ExtendableOptionsExtensions.GetOperationProperties' extension from Core to prevent this patch to force an update to Core 7.7 (as this has also been fixed on 7.5 and 7.6).
         public static ReadOnlyContextBag GetOperationProperties(this ContextBag contextBag)
         {
             if (contextBag.TryGet("NServiceBus.OperationProperties", out ContextBag operationProperties))
