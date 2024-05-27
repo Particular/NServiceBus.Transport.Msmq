71b5cd3d6fff3e3fcaf5b9af310cff0e6cb74fbb
Andreas Öhlund <andreas.ohlund@particular.net>
2024-05-27 08:55:10 +0200

    Do not emit BOM when serializing message headers (#710)
    * Add failing test
    
    * Add test to show that v2 and below didn't emit BOM
    
    * Link to v1 and v2
    
    * Test names
    
    * Repro user issue
    
    * Uncommented fix and caching object as suggested by Daniel
    
    * EOF and moving HeaderSerializerXmlWriterSettings down
    
    * Update tests
    
    * Also cache writer settings
    
    * readonly
    
    ---------
    
    Co-authored-by: Ramon Smits <ramon.smits@gmail.com>
    Co-authored-by: danielmarbach <daniel.marbach@openplace.net>

--- src/NServiceBus.Transport.Msmq.Tests/MsmqUtilitiesTests.cs
+++ src/NServiceBus.Transport.Msmq.Tests/MsmqUtilitiesTests.cs
@@ -2,6 +2,10 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml;
     using NUnit.Framework;
     using Particular.Msmq;
     using Support;
@@ -117,6 +121,68 @@
             Dictionary<string, string> headers = MsmqUtilities.ExtractHeaders(message);
 
             Assert.AreEqual(expected, headers["NServiceBus.ExceptionInfo.Message"]);
+        }
+
+        [Test]
+        public void Should_not_emit_BOM_when_serializing_headers_for_backwards_compatibility()
+        {
+            // This test ensures that older versions of the transport can still parse the headers since that code can't handle a BOM 
+            // v1.0.X - https://github.com/Particular/NServiceBus.Transport.Msmq/blob/1.0.2/src/NServiceBus.Transport.Msmq/MsmqUtilities.cs#L94
+            // v6 with msmq bundled - https://github.com/Particular/NServiceBus/blob/6.5.10/src/NServiceBus.Core/Transports/Msmq/MsmqUtilities.cs#L98
+            // v5 with msmq bundled - https://github.com/Particular/NServiceBus/blob/5.2.26/src/NServiceBus.Core/Transports/Msmq/MsmqUtilities.cs#L199
+            // Note: As of v1.1 the transport can handle a BOM due to this change - https://github.com/Particular/NServiceBus.Transport.Msmq/pull/125
+
+            var message =
+                MsmqUtilities.Convert(
+                    new OutgoingMessage("message id",
+                        new Dictionary<string, string> { { "some-header", "some value" } }, Array.Empty<byte>()));
+
+            var encodingWithBOM = new UTF8Encoding(true);
+            var preamble = encodingWithBOM.GetPreamble();
+
+            Assert.AreNotEqual(preamble, message.Extension.Take(preamble.Length));
+        }
+
+        [Test]
+        public void Default_usage_of_xml_serializer_does_not_emit_bom()
+        {
+            // Older versions of the transport doesn't use XmlWriter settings and therefor no BOM is emitted. This test verified that behavior
+
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(string));
+
+            using (var stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, "test");
+
+                var seralizedHeaders = stream.ToArray();
+
+                var encodingWithBOM = new UTF8Encoding(true);
+                var preamble = encodingWithBOM.GetPreamble();
+
+                Assert.AreNotEqual(preamble, seralizedHeaders.Take(preamble.Length));
+            }
+        }
+
+        [Test]
+        public void Default_xmlwritersettings_emit_bom()
+        {
+            // This test docments that the default XmlWriterSettings emits a BOM by default
+
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(string));
+
+            using (var stream = new MemoryStream())
+            {
+                using var writer = XmlWriter.Create(stream, new XmlWriterSettings());
+
+                serializer.Serialize(writer, "test");
+
+                var seralizedHeaders = stream.ToArray();
+
+                var encodingWithBOM = new UTF8Encoding(true);
+                var preamble = encodingWithBOM.GetPreamble();
+
+                Assert.AreEqual(preamble, seralizedHeaders.Take(preamble.Length));
+            }
         }
     }
 }
