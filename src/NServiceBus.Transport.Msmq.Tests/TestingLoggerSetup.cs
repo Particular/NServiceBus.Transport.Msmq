a3fdf26a7ffcb42f423c937846ea56f010264565
Tomek Masternak <tomasz.masternak@particular.net>
2024-06-04 17:09:57 +0200

    make sure testing logger is initialized before any static logger field gets accessed

---
+++ src/NServiceBus.Transport.Msmq.Tests/TestingLoggerSetup.cs
@@ -0,0 +1,13 @@
+﻿namespace NServiceBus.Transport.Msmq.Tests
+{
+    using Logging;
+    using NUnit.Framework;
+    using Testing;
+
+    [SetUpFixture]
+    public class TestingLoggerSetup
+    {
+        [OneTimeSetUp]
+        public void SetupTestingLogger() => LogManager.Use<TestingLoggerFactory>();
+    }
+}
