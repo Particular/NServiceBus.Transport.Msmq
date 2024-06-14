dd0ad2dd9c94cc2961d3c7cc8118a55712a64e02
Mike Minutillo <mike.minutillo@particular.net>
2024-06-14 12:04:50 +0800

    Test delayed delivery without installers

---
+++ src/NServiceBus.Transport.Msmq.AcceptanceTests/DelayedDelivery/When_endpoint_starts_without_installers_and_infrastructure_configured.cs
@@ -0,0 +1,83 @@
+﻿namespace NServiceBus.AcceptanceTests.DelayedDelivery
+{
+    using System;
+    using System.Threading.Tasks;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTesting.Customization;
+    using NServiceBus.AcceptanceTests.EndpointTemplates;
+    using NServiceBus.Configuration.AdvancedExtensibility;
+    using NUnit.Framework;
+
+    class When_endpoint_starts_without_installers_and_infrastructure_configured : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Delayed_delivery_works()
+        {
+            Requires.DelayedDelivery();
+
+            _ = await Scenario.Define<Context>()
+                .WithEndpoint<Receiver>()
+                .WithEndpoint<Sender>()
+                .Done(c => c.Done)
+                .Run();
+
+
+            _ = await Scenario.Define<Context>()
+                .WithEndpoint<Receiver>(s => s.CustomConfig(cfg => cfg.GetSettings().Set("Installers.Enable", false)))
+                .WithEndpoint<Sender>(s => s.CustomConfig(cfg => cfg.GetSettings().Set("Installers.Enable", false))
+                    .When(
+                        (session, _) =>
+                        {
+                            var sendOptions = new SendOptions();
+                            sendOptions.DelayDeliveryWith(TimeSpan.FromSeconds(2));
+                            return session.Send(new DelayedMessage(), sendOptions);
+                        }))
+                .Done(c => c.Done)
+                .Run();
+        }
+
+        class DelayedMessage : IMessage
+        {
+
+        }
+
+        class Sender : EndpointConfigurationBuilder
+        {
+            public Sender()
+            {
+                EndpointSetup<DefaultServer>(cfg =>
+                    cfg.ConfigureRouting().RouteToEndpoint(typeof(DelayedMessage), typeof(Receiver))
+                );
+            }
+        }
+
+        class Receiver : EndpointConfigurationBuilder
+        {
+            public Receiver()
+            {
+                EndpointSetup<DefaultServer>();
+            }
+
+            class DelayedMessageHandler : IHandleMessages<DelayedMessage>
+            {
+                readonly Context scenarioContext;
+
+                public DelayedMessageHandler(Context scenarioContext)
+                {
+                    this.scenarioContext = scenarioContext;
+                }
+
+                public Task Handle(DelayedMessage message, IMessageHandlerContext context)
+                {
+                    scenarioContext.Done = true;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+
+        class Context : ScenarioContext
+        {
+            public bool Done { get; set; }
+        }
+    }
+}
